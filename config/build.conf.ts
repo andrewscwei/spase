d18fb0caab36ce968b584ff4a8ea82d3d9dbfaa0
Andrew Wei <andrewscwei@gmail.com>
2019-07-03 16:58:25 -0700

    Bundle with Webpack

---
+++ config/build.conf.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const cwd = path.join(__dirname, '../');
+
+const config: Configuration = {
+  devtool: 'source-map',
+  entry: {
+    index: path.join(cwd, 'src/index.ts'),
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.ts$/,
+      use: [{
+        loader: 'ts-loader',
+      }],
+    }],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(cwd, 'build'),
+    sourceMapFilename: '[file].map',
+    libraryTarget: 'umd',
+    library: 'Spase',
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  stats: {
+    colors: true,
+    errors: true,
+    errorDetails: true,
+    maxModules: 0,
+    modules: true,
+    moduleTrace: true,
+    publicPath: true,
+    reasons: true,
+    timings: true,
+  },
+  target: 'web',
+};
+
+export default config;
