437aa110262b33895daf62df376b1120d5a5aa8d
Andrew Wei <andrewscwei@gmail.com>
2019-11-02 02:12:24 -0700

    Update demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -12,15 +12,17 @@ const bNode = document.getElementById('b');
 
 window.localStorage.debug = 'spase*';
 
-debug('Viewport', Rect.fromViewport());
+window.addEventListener('click', () => {
+  debug('Viewport', Rect.fromViewport());
 
-debug('<body>', Rect.from(window));
-debug('<body> (with overflow)', Rect.from(window, { overflow: true }));
-debug('<body> (from children)', Rect.fromChildrenOf(window));
+  debug('<body>', Rect.from(window));
+  debug('<body> (with overflow)', Rect.from(window, { overflow: true }));
+  debug('<body> (from children)', Rect.fromChildrenOf(window));
 
-debug('#main', Rect.from(mainNode));
-debug('#main (with overflow)', Rect.from(mainNode, { overflow: true }));
-debug('#main (from children)', Rect.fromChildrenOf(mainNode));
+  debug('#main', Rect.from(mainNode));
+  debug('#main (with overflow)', Rect.from(mainNode, { overflow: true }));
+  debug('#main (from children)', Rect.fromChildrenOf(mainNode));
 
-debug('#b', Rect.from(bNode));
-debug('#b (relative to #main)', Rect.from(bNode, { reference: mainNode }));
+  debug('#b', Rect.from(bNode));
+  debug('#b (relative to #main)', Rect.from(bNode, { reference: mainNode }));
+});
