ab6139c70480bbd1fcec343fbeda09c2eba2a63b
Andrew Wei <andrewscwei@gmail.com>
2020-05-11 13:09:38 -0700

    Fix deploying to GitHub pages in CD workflow

--- .github/workflows/deploy_to_gh_pages.sh
+++ .github/workflows/deploy_to_gh_pages.sh
@@ -3,28 +3,27 @@
 # Required environment variables:
 # - GH_USER: Name of GitHub user to use for the commit
 # - GH_PAGES_DIR: Directory (relative to project root) to deploy to GitHub Pages
+# - GH_TOKEN: GitHub auth token for write permissions
 
-__GH_PAGES_DIR__=${GH_PAGES_DIR:-.gh-pages}
-__GH_USER__=${GH_USER:-$GITHUB_ACTOR}
-__ORIGIN_URL__=`git config --get remote.origin.url`
+GH_PAGES_DIR=${GH_PAGES_DIR:-.gh-pages}
+GH_USER=${GH_USER:-$GITHUB_ACTOR}
+ORIGIN_URL="https://$GH_USER:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
-if [ `git branch | grep gh-pages` ]; then
-  git branch -D gh-pages
-fi
-
+# Checkout new branch.
+if [ `git branch | grep gh-pages` ]; then git branch -D gh-pages; fi
 git checkout -b gh-pages
-npm run pages
 
-# Move generated files to root and delete everything else.
-find . -maxdepth 1 ! -name '.' ! -name '..' ! -name ${__GH_PAGES_DIR__} ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv ${__GH_PAGES_DIR__}/* .
-rm -R ${__GH_PAGES_DIR__}/
+# Build and move generated files to root, then delete everything else.
+npm run pages
+find . -maxdepth 1 ! -name '.' ! -name '..' ! -name ${GH_PAGES_DIR} ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv ${GH_PAGES_DIR}/* .
+rm -R ${GH_PAGES_DIR}/
 
 # Push to gh-pages.
-git config user.name "$__GH_USER__"
-git config user.email "$__GH_USER__@users.noreply.github.com"
+git config user.name "$GH_USER"
+git config user.email "$GH_USER@users.noreply.github.com"
 git add -fA
 git commit --allow-empty -m "[SKIP CI] $(git log -1 --pretty=%B)"
-git push -f $__ORIGIN_URL__ gh-pages
+git push -f $ORIGIN_URL gh-pages
 
 echo "Successfully published docs to GitHub Pages"
