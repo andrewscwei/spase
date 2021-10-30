8587455056b34b04b8aac06c94b90cf3fb27fef2
Andrew Wei <andrewscwei@gmail.com>
2021-10-30 10:01:52 -0700

    Add license info

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+/** @license Spase
+ * © Andrew Wei
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
 export { default as Point } from './core/Point'
 export { default as Rect } from './core/Rect'
 export { default as Size } from './core/Size'
 export * from './types'
 export { default as getFOV } from './utils/getFOV'
 export { default as hitTest } from './utils/hitTest'
-
