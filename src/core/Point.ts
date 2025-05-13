9bb902a44dfe9c5f85ab1ea8da1316061b84538a
Andrew Wei <andrewscwei@gmail.com>
2025-05-13 13:32:04 -0700

    feat: Add toString to Point, Rect, and Size

--- src/core/Point.ts
+++ src/core/Point.ts
@@ -184,6 +184,17 @@ export namespace Point {
   }
 
   /**
+   * Returns the string representation of a {@link Point}
+   *
+   * @param point The {@link Point} to convert.
+   *
+   * @returns The resulting string.
+   */
+  export function toString(point: Point): string {
+    return `Point(x=${point.x}, y=${point.y})`
+  }
+
+  /**
    * Returns the JSON representation of a {@link Point}.
    *
    * @param point The {@link Point} to convert.
--- src/core/Size.ts
+++ src/core/Size.ts
@@ -184,6 +184,17 @@ export namespace Size {
   }
 
   /**
+   * Returns the string representation of a {@link Size}
+   *
+   * @param size The {@link Size} to convert.
+   *
+   * @returns The resulting string.
+   */
+  export function toString(size: Size): string {
+    return `Size(width=${size.width}, height=${size.height})`
+  }
+
+  /**
    * Returns the JSON representation of a {@link Size}.
    *
    * @param size The {@link Size} to convert.
